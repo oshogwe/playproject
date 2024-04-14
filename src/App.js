3c8659bfd7b206b1b1168af7dbf96dd1db39ac51
oshogwe ikekhai <osho/ikekhai@yahoo.com>
2024-04-14 03:31:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [superPower, setSuperPower] = useState("");
+  const [displayCharacter, setDisplayCharacter] = useState(false);
+
+  return ( 
+  <div className='App'>  
+    <h1>Build a Hero</h1>
+    <div>
+      <label>Name:</label>
+      <input
+        type='text' 
+        style={{margin:10}} 
+        onChange={(event)=>{setName(event.target.value)}}/>
+
+      <label>Age:</label>
+      <input 
+      type='number' 
+      style={{margin:10}}
+      onChange={(event)=>{setAge(event.target.value)}}/>
+      
+      <label>Height:</label>
+      <input 
+      type='number' 
+      style={{margin:10}}
+      onChange={(event)=>{setHeight(event.target.value)}}/> 
+
+      <label>SuperPower:</label>
+      <input 
+      type='text' 
+      style={{margin:10}}
+      onChange={(event)=>{setSuperPower(event.target.value)}}/>
     </div>
-  );
-}
+    <button 
+    onClick={()=>{setDisplayCharacter(true)}}>
+    Display Character</button>
+    <div>
+      <h1>Hero Info</h1>
+      {displayCharacter && (
+        <ul>
+          <li>Name: {name}</li>
+          <li>Age: {age}</li>
+          <li>Height: {height}</li>
+          <li>Strength: {superPower}</li>
+        </ul>
+      )}
+    </div>  
+  </div>
+);}
 
 export default App;
